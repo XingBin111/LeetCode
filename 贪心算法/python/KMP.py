2918594cba468fec0f5ffc3fc054a1d99cfa2830
邢斌 <xingb001@cmft.com>
2020-05-28 16:19:22 +0800

    KMP

--- 贪心算法/python/KMP.py
+++ 贪心算法/python/KMP.py
@@ -1,3 +1,51 @@
-# coding = utf-8
-#__author__ = ''
-#__date__ = '2020/5/28'
+"""
+KMP算法用于字符串匹配, 首先约定pat表示模式串, 长度为M. txt表示文本串, 长度为N. KMP算法就是在txt中查找子串pat,
+如果存在, 就返回这个子串在txt中的起始索引, 不存在返回-1
+"""
+
+
+def violence_kmp(pat, txt):
+    """
+    使用暴力算法求解. 时间效率为O(MN), 空间复杂度为O(1)
+    使用该解法求pat = "aaab", txt = "aaacaaab", txt中字母"c"被反复比较, 但其实pat中根本不含"c", 所以没必要比较.
+    """
+    M = len(pat)
+    N = len(txt)
+    for i in range(N-M+1):
+        if txt[i:i+M] == pat:
+            return i
+    return -1
+
+
+def kmp(pat, txt):
+    """
+    kmp算法永不回退txt的指针(不会重复扫描txt), 而是借助dp数组中存储的信息把pat移动到正确的位置继续匹配, 以空间换时间,
+    所以时间复杂度为O(N), 空间复杂度为O(M)
+    """
+    M = len(pat)
+    N = len(txt)
+    dp_table = {}
+    txt_e = list(set(txt))
+    for e in set(txt_e):
+        dp_table[(0, e)] = 0
+    dp_table[(0, pat[0])] = 1
+    x = 0
+    for j in range(1, M):
+        for c in range(len(txt_e)):
+            e = txt_e[c]
+            dp_table[(j, e)] = dp_table[(x, e)]
+        dp_table[(j, pat[j])] = j + 1
+        x = dp_table[(x, pat[j])]   # 很关键, 还不是很懂
+
+    j = 0
+    for i in range(N):
+        j = dp_table[(j, txt[i])]
+        if j == M:
+            return i - M + 1
+    return -1
+
+
+if __name__ == "__main__":
+    pat = "ABABC"
+    txt = "CABAABABAC"
+    print(kmp(pat, txt))
--- 贪心算法/python/intervalSchedule.py
+++ 贪心算法/python/intervalSchedule.py
@@ -1,3 +1,51 @@
-# coding = utf-8
-#__author__ = ''
-#__date__ = '2020/5/28'
+"""
+今天你能够参加好几个活动, 每个活动开始和结束的时间用[start, end]表示, 你不能同时参加2个活动,
+问题: 你一天最多能参加几个活动?
+显然是求出这些时间区间的最大不相交子集
+
+解决思路:
+    1. 从区间集合intvs选择一个区间x, x是所有区间中结束最早的(end最小)
+    2. 把所有与x相交的区间从intvs中删除.
+    3. 重复1和2, 直到区间集合intvs为空
+时间复杂度为O(N)
+"""
+
+
+def interval_schedule_while(intvs):
+    intvs = sorted(intvs, key=lambda e: e[1])
+    start = 0
+    n = len(intvs)
+    valid_interval_num = 0
+    while start < n:
+        jump = 0
+        for i in range(start, n):
+            if intvs[i][0] < intvs[start][1]:
+                jump += 1
+        start += jump
+        valid_interval_num += 1
+    return valid_interval_num
+
+
+def interval_schedule_for(intvs):
+    intvs = sorted(intvs, key=lambda e: e[1])
+    n = len(intvs)
+    valid_interval_num = 1
+    x_end = intvs[0][1]
+    for i in range(n):
+        if intvs[i][0] >= x_end:
+            valid_interval_num += 1
+            x_end = intvs[i][1]
+    return valid_interval_num
+
+
+def erase_overlap_intervals(intvs):
+    """
+    给定一个区间的集合, 找到需要移除区间的最小数量, 使剩余区间互不重叠
+    """
+    return len(intvs) - interval_schedule_for(intvs)
+
+
+
+if __name__ == "__main__":
+    intvs = [[1, 3], [2, 4], [3, 6]]
+    print(interval_schedule_for(intvs))

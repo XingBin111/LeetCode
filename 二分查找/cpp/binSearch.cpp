57dc896740132b98747570edd8ca22b7b17794f1
邢斌 <xingb001@cmft.com>
2020-06-01 11:14:27 +0800

    二分查找cpp

---
+++ 二分查找/cpp/binSearch.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binSearch(const vector<int>& nums, int target)
+{
+    int left = 0;
+    int right = nums.size();
+    while(left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else if (nums[mid] > target)
+        {
+            right = mid;
+        }
+    }
+    return -1;
+}
+
+int binSearchLeftBound(const vector<int>& nums, int target)
+{
+    int left = 0;
+    int right = nums.size();
+    while(left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target)
+            right = mid;
+        else if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else if (nums[mid] > target)
+        {
+            right = mid;
+        }
+    }
+    if(left==nums.size() || nums[left] != target)
+        return -1;
+    return left;
+}
+
+int binSearchRightBound(const vector<int>& nums, int target)
+{
+    int left = 0;
+    int right = nums.size();
+    while(left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target)
+            left = mid + 1;
+        else if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else if (nums[mid] > target)
+        {
+            right = mid;
+        }
+    }
+    if(right==0 || nums[right-1] != target)
+        return -1;
+    return right-1;
+}
+
+void usage(const vector<int>& nums, const vector<int>& target, int (pfunc)(const vector<int>&, int))
+{   
+    cout << "nums: ";
+    for(int i=0; i<nums.size(); i++)
+        cout << nums[i] << ", ";
+    cout << endl;
+    cout << "find target in nums: \n";
+    for(int i=0; i<target.size(); i++)
+    {   
+        cout << "target: " << target[i] << ", index:" << pfunc(nums, target[i]) << endl;
+    }
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 2, 2, 3, 4, 6, 7, 8};
+    vector<int> target = {0, 2, 4, 5, 9};
+    
+    cout << "------------usage of binSearch------------\n";
+    usage(nums, target, binSearch);
+    
+    cout << endl;
+
+    cout << "------------usage of binSearchLeftBound------------\n";
+    usage(nums, target, binSearchLeftBound);
+
+    cout << endl;
+
+    cout << "------------usage of binSearchRightBound------------\n";
+    usage(nums, target, binSearchRightBound);
+
+    return 0;
+}

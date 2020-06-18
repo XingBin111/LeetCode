4bca7c2a74eb0ff048d19b17f4d023be90f67b4d
邢斌 <xingb001@cmft.com>
2020-06-18 18:21:53 +0800

    kmp python

--- 贪心算法/cpp/KMP.cpp
+++ 贪心算法/cpp/KMP.cpp
@@ -6,13 +6,13 @@
 
 using namespace std;
 
-int KMP(const string& pat, const string& txt)
+// 暴力方法创建, O(M^2)
+vector<int> construct_table_brute(const string& pat)
 {
     int M = pat.size();
-    int N = txt.size();
 
     // 构造最长公共前后缀, 这是KMP算法最核心的部分
-    vector<int> dp_table(M, 0);
+    vector<int> dp_table(M, -1);
     for(int i=1; i<M; i++)
     {
         string tmp = pat.substr(0, i);
@@ -27,19 +27,67 @@ int KMP(const string& pat, const string& txt)
         }
         dp_table[i] = res;
     }
-    int j = 0;
-    for(int i=0; i<txt.size(); i++)
-    {
-        if(txt[i] != pat[j])
-            j = dp_table[j];
-        else
-        {   
-            j++;
-            if(j == pat.size())
-                return i - pat.size() + 1;
-        }
+    // for(auto& x: dp_table)
+    //     cout << x << endl;
+    return dp_table;
+}
+
+// 迭代创建, O(M)
+vector<int> construct_table_iteration(const string& pat)
+{
+    int M = pat.size();
+
+    vector<int> dp_table(M, -1);
+
+    // 该版本理解起来比较困难, 所以替换
+    // int t = -1;
+    // int j = 0;
+    // while(j < M - 1)
+    // {
+    //     if(0 > t || pat[j] == pat[t])
+    //     {
+    //         j++;
+    //         t++;
+    //         dp_table[j] = t;
+    //     }
+    //     else
+    //         t = dp_table[t];
+    // }
+    for(int j=1; j < M; j++)
+    {   
+        int t = dp_table[j-1];
+        while(t >= 0 && pat[j-1] != pat[t])
+            t = dp_table[t];
+        dp_table[j] = t + 1;
     }
-    return -1;
+
+    // for(auto& x: dp_table)
+    //     cout << x << endl;
+    return dp_table;
+}
+
+// 效率为O(N)
+int KMP(const string& pat, const string& txt)
+{
+    int M = pat.size();
+    int N = txt.size();
+
+    // 构造最长公共前后缀, 这是KMP算法最核心的部分
+    vector<int> dp_table = construct_table_iteration(pat);
+    
+    int j = 0;
+    int i = 0;
+    while(j < M && i < N)
+    {
+        if(txt[i] == pat[j] || 0 > j)
+        {
+            i++;
+            j++;
+        }
+        else
+            j = dp_table[j];
+    }
+    return i - j;   
 }
 
 int main()

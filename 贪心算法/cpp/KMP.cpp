3be15acd817284b9a5359b6b87de86ae74b0dc96
邢斌 <xingb001@cmft.com>
2020-06-19 10:57:40 +0800

    kmp cpp

--- 贪心算法/cpp/KMP.cpp
+++ 贪心算法/cpp/KMP.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
@@ -39,41 +38,67 @@ vector<int> construct_table_iteration(const string& pat)
 
     vector<int> dp_table(M, -1);
 
-    // 该版本理解起来比较困难, 所以替换
-    // int t = -1;
-    // int j = 0;
-    // while(j < M - 1)
-    // {
-    //     if(0 > t || pat[j] == pat[t])
-    //     {
-    //         j++;
-    //         t++;
-    //         dp_table[j] = t;
-    //     }
-    //     else
-    //         t = dp_table[t];
-    // }
-    for(int j=1; j < M; j++)
-    {   
-        int t = dp_table[j-1];
-        while(t >= 0 && pat[j-1] != pat[t])
+    // t = dp_table[j]
+    int t = -1;
+    int j = 0;
+    while(j < M - 1)
+    {
+        if(0 > t || pat[j] == pat[t])
+        {
+            j++;
+            t++;
+            dp_table[j] = t;
+        }
+        else
             t = dp_table[t];
-        dp_table[j] = t + 1;
     }
+    // for(int j=1; j < M; j++)
+    // {   
+    //     int t = dp_table[j-1];
+    //     while(t >= 0 && pat[j-1] != pat[t])
+    //         t = dp_table[t];
+    //     dp_table[j] = t + 1;
+    // }
 
-    // for(auto& x: dp_table)
-    //     cout << x << endl;
+    for(int i=0; i<dp_table.size(); i++)
+        cout << dp_table[i] << endl;
     return dp_table;
 }
 
-// 效率为O(N)
+// 迭代创建, O(M)
+vector<int> construct_table_better(const string& pat)
+{
+    int M = pat.size();
+
+    vector<int> dp_table(M, -1);
+
+    int t = -1;
+    int j = 0;
+    while(j < M - 1)
+    {
+        if(0 > t || pat[j] == pat[t])
+        {
+            j++;
+            t++;
+            dp_table[j] = (pat[j] != pat[t] ? t : dp_table[t]);
+        }
+        else
+            t = dp_table[t];
+    }
+
+    for(int i=0; i<dp_table.size(); i++)
+        cout << dp_table[i] << endl;
+    return dp_table;
+}
+
+// 匹配效率为O(N), 所以总的效率为O(m+n)
 int KMP(const string& pat, const string& txt)
 {
     int M = pat.size();
     int N = txt.size();
 
     // 构造最长公共前后缀, 这是KMP算法最核心的部分
-    vector<int> dp_table = construct_table_iteration(pat);
+    vector<int> dp_table = construct_table_better(pat);
     
     int j = 0;
     int i = 0;
@@ -93,9 +118,10 @@ int KMP(const string& pat, const string& txt)
 int main()
 {
     string pat = "ABABC";
+    // string pat = "00010";
     string txt = "CABAABABCC";
     int res = KMP(pat, txt);
-    cout << res << endl;
-    cout << txt.substr(res, pat.size()) << endl;
+    // cout << res << endl;
+    // cout << txt.substr(res, pat.size()) << endl;
     return 0;
 }

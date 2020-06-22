a4ea520c1c0f2efe2df022cd49334bbad3b993e9
邢斌 <xingb001@cmft.com>
2020-06-22 16:55:09 +0800

    HashMap python

--- DengJH/python/HashMap.py
+++ DengJH/python/HashMap.py
@@ -1,56 +1,88 @@
 class HashMap:
     def __init__(self, volume=5):
         self.volume = volume
-        self.indices = [-1] * self.volume
-        self.entities = []
+
+        # None表示空桶, -1表示lazy remove
+        self.entities = [None] * self.volume
+        self.size = 0
+        self.load_factor = 0.5
 
     def insert(self, key, val):
         hash_val = hash(key)
-        entity = [hash_val, key, val]
-        indice = hash_val % self.volume
+        idx = hash_val % self.volume
+        key_in_map = False
+        # 找到空桶或lazy remove桶时就结束循环
+        while self.entities[idx] is not None:
 
-        if self.indices[indice] != -1:
-            # 如果哈希值相等, 就更新val
-            if self.entities[self.indices[indice]][0] == hash_val:
-                self.entities[self.indices[indice]][2] = val
-                return
-            # 否则就处理冲突
+            # 找到lazy remove
+            if self.entities[idx][0] == -1:
+                break
+            # 找到相同的key
+            elif self.entities[idx][1] == key:
+                key_in_map = True
+                break
             else:
-                # 寻找新的插入位置
-                print("conflict:", key)
-                while self.indices[indice] != -1:
-                    indice += 1
-                    if indice >= self.volume:
-                        indice %= self.volume
+                idx += 1
+                idx = idx % self.volume
 
-        self.indices[indice] = len(self.entities)
-        self.entities.append(entity)
+        self.entities[idx] = [hash_val, key, val]
+        if not key_in_map:
+            self.size += 1
 
-        if len(self.entities) / self.volume > 0.5:
-            self.expand()
-        return
+            if self.size / self.volume > self.load_factor:
+                self.expand()
 
     def expand(self):
-        print("expand")
-        self.volume = 2 * self.volume
-        self.indices = [-1] * self.volume
-        old_entities = self.entities.copy()
-        self.entities = []
+        new_volume = self.volume * 2
+        new_entities = [None] * new_volume
+        for i, e in enumerate(self.entities):
+            if e is not None and e[0] != -1:
+                new_entities[i] = e
 
-        for hash_val, key, val in old_entities:
-            self.insert(key, val)
+        self.entities = [None] * new_volume
+        self.volume = new_volume
+        self.size = 0
+        for i, e in enumerate(new_entities):
+            if e is not None:
+                self.insert(e[1], e[2])
 
     def get(self, key):
         hash_val = hash(key)
-        indice = hash_val % self.volume
-        if self.indices[indice] == -1:
-            return None
-        else:
-            while self.entities[self.indices[indice]][0] != hash_val:
-                indice += 1
-                if indice >= self.volume:
-                    indice %= self.volume
-            return self.entities[self.indices[indice]][2]
+        idx = hash_val % self.volume
+
+        i = 0
+        # 沿着非空桶的链进行查找
+        while self.entities[idx] is not None:
+            if self.entities[idx][1] == key and self.entities[idx][0] != -1:
+                return self.entities[idx][2]
+            else:
+                i += 1
+                idx += 1
+                idx = idx % self.volume
+            if i >= self.volume:
+                break
+        print("{} not in map".format(key))
+        return None
+
+    def delete(self, key):
+        hash_val = hash(key)
+        idx = hash_val % self.volume
+
+        i = 0
+        # 沿着非空桶的链进行查找
+        while self.entities is not None:
+            if self.entities[idx][1] == key:
+                self.entities[idx][0] = -1
+                self.size -= 1
+                return
+            else:
+                i += 1
+                idx += 1
+                idx = idx % self.volume
+            if i >= self.volume:
+                break
+        print("{} not in map".format(key))
+        return
 
 
 if __name__ == "__main__":
@@ -66,3 +98,10 @@ if __name__ == "__main__":
     d.insert('a', 2)
 
     print(d.get('a'))
+
+    d.delete("a")
+
+    d.get("a")
+
+    d.insert("d", 4)
+    d.insert("d", 5)
